ce16f05d1266a388b36f61ca2e84a38491373154
David Herney <davidherney@gmail.com>
2023-06-23 02:56:31 -0500

    first commit

---
+++ lang/en/customcertelement_otherdates.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of the customcert module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'customcertelement_otherdates', language 'en'.
+ *
+ * @package    customcertelement_otherdates
+ * @copyright  2023 David Herney @ BambuCo
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['firstmodview'] = 'First module view';
+$string['dateformat'] = 'Date format';
+$string['dateformat_help'] = 'This is the format of the date that will be displayed';
+$string['dateitem'] = 'Date item';
+$string['dateitem_help'] = 'This will be the date that is printed on the certificate';
+$string['pluginname'] = 'Other dates';
+$string['privacy:metadata'] = 'The Other dates plugin does not store any personal data.';
+$string['numbersuffix_nd_as_in_second'] = 'nd';
+$string['numbersuffix_rd_as_in_third'] = 'rd';
+$string['numbersuffix_st_as_in_first'] = 'st';
+$string['userdateformat'] = 'User date format';
